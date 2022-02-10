d8649e4e73b3607154dca31eb270016b6b404657
CodyLuckmann <cluckmann@live.com>
2022-02-10 10:08:52 -0600

    feat: NavBar created

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,40 @@
+import React, { useReducer } from 'react';
+import { Link } from 'react-router-dom';
+
+
+function NavBar({user}) {
+    
+
+    function logout() {
+        localStorage.removeItem('token');
+        window.location = '/'
+    }
+
+    return(
+        <div>
+            {user && <h2>Welcome to Golf Tracker!</h2>}
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                {!user &&
+                    <React.Fragment>
+                        <li>
+                            <Link to='/register'>Register</Link>
+                        </li>
+                        <li>
+                            <Link to ='/login'>Login</Link>
+                        </li>
+                    </React.Fragment>
+                }
+                {user &&
+                    <React.Fragment>
+                        <button onClick={logout}>Log Out</button>
+                    </React.Fragment>    
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default NavBar;
