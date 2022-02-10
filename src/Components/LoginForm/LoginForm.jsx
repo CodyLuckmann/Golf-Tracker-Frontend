279b97f64669a67ae4956919b77832d23d5bedb7
CodyLuckmann <cluckmann@live.com>
2022-02-10 09:29:33 -0600

    feat: created login form

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { React, useState } from 'react';
+
+
+const LoginForm = (props) => {
+
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleUserName = (e) => {
+        setUserName(e.target.value)
+    };
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let login = 
+            {
+                username: username,
+                password: password,
+            }
+
+        let response = await axios.post('http://127.0.0.1:8000/api/auth/login/', login);
+        localStorage.setItem('token', response.data.access);
+        window.location = '/'
+    };
+
+    return (
+        <div>
+            <div>
+                <hi>Login</hi>
+            </div>
+            <form>
+                <label>Username</label>
+                <input onChange={handleUserName}
+                value={username} type='text' />
+
+                <label>Password</label>
+                <input onChange={handlePassword}
+                value={password} type='password' />
+
+                <button onClick={handleSubmit} type='submit'>
+                    Submit
+                </button>
+            </form>
+        </div>
+
+    );
+}
+
+export default LoginForm;
