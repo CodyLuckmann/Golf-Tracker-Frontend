bb26c818597ec299afa9570d0dffd714c4c53945
CodyLuckmann <cluckmann@live.com>
2022-02-14 16:07:45 -0600

    feat: added round form to add data for a round of golf

---
+++ src/Components/RoundForm/RoundForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import AddHoleForm from "../AddHoleForm/AddHoleForm";
+
+const RoundForm = (props) =>{
+
+    const [allHoles, setAllHoles] = useState([]);
+  
+    function handleSubmit(newHole){
+        console.log(newHole)
+        //add newHole into 'allHoles' hook array
+        props.createPlayedHole(newHole);
+    }
+
+
+
+    const buildForms = ()=>{
+        let arrayOfForms = []
+        for(let i = 1; i < props.numOfHoles; i++){
+            arrayOfForms.push(<AddHoleForm key={i} hole={i} handleSubmit={handleSubmit} />)
+        }
+        return arrayOfForms;
+    }
+
+    return(
+        <div>
+            {buildForms()} 
+        </div>
+        
+        
+   
+
+    )
+
+
+   
+
+
+
+}
+export default RoundForm;
